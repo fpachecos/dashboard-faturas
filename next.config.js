662356dfa884c48ad938864bdd65fcaabc1d614c
Filipe Pacheco <filipe.pacheco@deepesg.com>
2025-12-14 14:28:22 -0300

    Fix: Excluir app-expo do build do Next.js sem desabilitar o Expo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ignorar arquivos do Expo durante o build
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config) => {
+    // Ignorar a pasta app-expo durante o build
+    config.module.rules.push({
+      test: /app-expo\/.*/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
